a748341f03ee334bd8476847a488bd613fa11d10
Alexey Fayzullov <alex.f@techery.io>
2017-11-23 12:39:50 +0200

    Add typescript support

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,20 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Shake or press menu button for dev menu'
+})
 
-export default class App extends Component<{}> {
+export interface MyAppProps { }
+export interface MyAppState { }
+
+export default class ReactPrototype extends React.Component<MyAppProps, MyAppState> {
   render() {
     return (
       <View style={styles.container}>
@@ -27,31 +25,31 @@ export default class App extends Component<{}> {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit App.js
+          To get started, edit App.tsx
         </Text>
         <Text style={styles.instructions}>
           {instructions}
         </Text>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
+const styles: any = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
